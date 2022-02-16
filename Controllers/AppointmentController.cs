85572e7b4457ede331c610f922aeff1ad04beb62
Irena Staneva <irenamst@gmail.com>
2022-02-16 10:37:00 +0200

    Add files via upload

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,32 @@
+﻿using Activity2c.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Activity2c.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        static List<AppointmentModel> appointments = new List<AppointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+        public IActionResult Create()
+        { 
+            return View();
+        }
+        public IActionResult Details(AppointmentModel appointment)
+        {
+            appointments.Add(appointment);
+            return View("Details",appointment);
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
